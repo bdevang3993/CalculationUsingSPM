3053d5affd9d77a4f1656c357248137abe68806e
Devang <bdevang3993@gmail.com>
2026-06-02 15:59:39 +0530

    Initial Commit

---
+++ CalculationOperation/CalculationOperation.swift
@@ -0,0 +1,34 @@
+//
+//  CalculationOperation.swift
+//  CalculationOperation
+//
+//  Created by apple on 02/06/26.
+//
+
+import Foundation
+
+public final class calculator {
+    
+   private init () {
+        
+    }
+  static public let shared = calculator()
+    
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func mul(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    public func div(a: Int, b: Int) -> Int {
+        return a / b
+    }
+    
+}
